e7adc2fe72caa793205b3e1a579f6db7f664b05e
Exequiel Deferrari <exedeferrari@gmail.com>
2019-06-13 16:34:17 -0300

    2019-06-13 16:33 - Commit: SocialNetworkView

--- SocialNetwork/src/SocialNetwork/View/SocialNetworkView.java
+++ SocialNetwork/src/SocialNetwork/View/SocialNetworkView.java
@@ -1,7 +1,54 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package SocialNetwork.View;
 
+import java.awt.Dimension;
 
-public class SocialNetworkView {
+/**
+ *
+ * @author deferrari.exequiel
+ */
+public class SocialNetworkView extends javax.swing.JFrame {
+
+   
+    public SocialNetworkView() {
+        initComponents();
+        setPreferredSize(new Dimension(1280, 720));
+    }
+
     
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 876, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 503, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SocialNetworkView().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
 }

2afe2abb5e3a20ec64038b764e823ac146739965
Exequiel Deferrari <exedeferrari@gmail.com>
2019-06-13 15:52:01 -0300

    2019-06-13 15:51 - Commit: instancia del view de Inicio de Sesion

--- SocialNetwork/src/InicioSesion/Controller/InicioSesionProcesarController.java
+++ SocialNetwork/src/InicioSesion/Controller/InicioSesionProcesarController.java
@@ -2,15 +2,19 @@
 package InicioSesion.Controller;
 
 import InicioSesion.Entities.Usuario;
+import InicioSesion.View.InicioSesionView;
 import SocialNetworl.Services.UsuarioService;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import static InicioSesion.Config.Variables.inicioSesionView;
 
 public class InicioSesionProcesarController {
     UsuarioService usuarioService;
     InicioSesionProcesarController(String accion) {
+        inicioSesionView = new InicioSesionView();
+        
         if(accion.equals("INICIO DE SESIÓN")){
             Usuario usuario = new Usuario();
             usuarioService = new UsuarioService();
